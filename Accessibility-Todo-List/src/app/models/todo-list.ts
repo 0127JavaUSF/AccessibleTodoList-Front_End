c9eff3b30301f99e4d3005b818e33007757a9ba1
Anvar <antnotes7@gmail.com>
2020-04-08 19:11:39 -0400

    add basic list display

--- Accessibility-Todo-List/src/app/models/todo-list.ts
+++ Accessibility-Todo-List/src/app/models/todo-list.ts
@@ -1,19 +1,13 @@
+import { TodoListItem } from './todo-list-item';
+
 export class TodoList {
-    
     id: number;
     title: String;
-    due: number;
-    note: String;
-    // url to external image
-    file: String;
+    items: Array<TodoListItem>;
 
-    constructor(id: number, title:String, due: number, note: String, file: String) {
+    constructor(id: number, title: String, items: Array<TodoListItem>) {
         this.id = id;
         this.title = title;
-        this.due = due;
-        this.note = note;
-        this.file = file;
+        this.items = items;
     }
-
-
 }
