3f215755490697e9d3d98c494b2982b89aa96c52
Khanh <knguye15.ccp@gmail.com>
2020-04-08 20:04:46 -0400

    landing components add

--- Accessibility-Todo-List/src/app/app.module.ts
+++ Accessibility-Todo-List/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderPanelComponent } from './header-panel/header-panel.component';
+import { MainLandingComponent } from './main-landing/main-landing.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderPanelComponent,
+    MainLandingComponent
   ],
   imports: [
     BrowserModule,
