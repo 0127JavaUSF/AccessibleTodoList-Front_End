9044060e3e45768d315991bd15579fa219a0cda9
Jean - Louis Macle <jeanlouis.macle@gmail.com>
2020-04-13 13:30:54 -0400

    Work on code to enhance accessibility for persons with dyslexia

---
+++ Font-toggle.js
@@ -0,0 +1,53 @@
+function toggleFont(){
+    window.document.getElementsByTagName('body')[0].setAttribute("style","font-family:Comic Sans MS;");    
+}
+
+function toComic(){
+    window.document.getElementsByTagName('body')[0].setAttribute("style","font-family:Comic Sans MS;");    
+}
+
+function toArial(){
+    window.document.getElementsByTagName('body')[0].setAttribute("style","font-family:Arial;");    
+}
+
+function toTahoma(){
+    window.document.getElementsByTagName('body')[0].setAttribute("style","font-family:Tahoma");    
+}
+
+function toVerdana(){
+    window.document.getElementsByTagName('body')[0].setAttribute("style","font-family:Verdana");    
+}
+
+function toGothic(){
+    window.document.getElementsByTagName('body')[0].setAttribute("style","font-family:Gothic");    
+}
+
+function toTrebuchet(){
+    window.document.getElementsByTagName('body')[0].setAttribute("style","font-family:Trebuchet");    
+}
+
+function toBookman(){
+    window.document.getElementsByTagName('body')[0].setAttribute("style","font-family:Bookman");    
+}
+
+function toCourier(){
+    window.document.getElementsByTagName('body')[0].setAttribute("style","font-family:Courier");    
+}
+
+function toGeorgia(){
+    window.document.getElementsByTagName('body')[0].setAttribute("style","font-family:Georgia");    
+}
+
+function toHelvetica(){
+    window.document.getElementsByTagName('body')[0].setAttribute("style","font-family:Helvetica");    
+}
+
+function toImpact(){
+    window.document.getElementsByTagName('body')[0].setAttribute("style","font-family:Impact");    
+}
+
+function toTimes(){
+    window.document.getElementsByTagName('body')[0].setAttribute("style","font-family:Times");    
+}
+
+
