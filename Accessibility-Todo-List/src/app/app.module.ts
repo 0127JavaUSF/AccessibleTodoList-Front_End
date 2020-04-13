f272da54ec8f15f6078dc5629d85a74a49b12fd8
Daniel <chateaustdanielle@gmail.com>
2020-04-13 13:23:03 -0400

    Created Login and Registration Page

--- Accessibility-Todo-List/src/app/app.module.ts
+++ Accessibility-Todo-List/src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { LoginComponent } from './components/login/login.component';
+import { RegistrationComponent } from './components/registration/registration.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+//import { ModelsComponent } from './components/models/user';
+//import { ServicesComponent } from './components/services/user.service';
+//import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegistrationComponent,
+    NavBarComponent,
+    //ModelsComponent,
+    //ServicesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    //NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
